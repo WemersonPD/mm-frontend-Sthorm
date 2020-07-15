0b90679cdc5b10be34388cb3e02c2bd442ebbd16
WemersonPD <wpdads10@gmail.com>
2020-07-15 02:36:00 -0300

    Estructuring basic signup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { SignupComponent } from './signup/signup.component';
 
 
 const routes: Routes = [
-
+  {path: '**', component: SignupComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {SignupModule} from './signup/signup.module'
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -11,6 +13,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    SignupModule
 
   ],
   providers: [],
